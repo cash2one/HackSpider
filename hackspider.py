b637ff30698604ce60d05646a01f61b508de46a0
GhostStar <root@zer.moe>
2016-03-17 14:50:25 +0800

    fix

--- hackspider.py
+++ hackspider.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import time
+import os
 from tld import get_tld
 from urllib.parse import urlparse
 import socket
@@ -9,12 +10,23 @@ from selenium.common.exceptions import TimeoutException
 
 # url = "http://icp.chinaz.com/searchs"
 # data = {'urls': 'qq.com', 'btn_search': '查询'}
+hackertime = "<td width=\"12%\"><b style=\"color:#0BA81E;\">(.*)</td>"
+hackerurl = "<td width=\"58%\" style=\"word-break:break-all\"><a href=\"(.*)\" target=\"_blank\""
+hackcntime = "<td width=\"16%\">(.*)</td>"
+hackcnweb = "<td width=\"7%\"><a href=\"(.*)\" target=\"_blank\">View</a></td>"
+hackcnu = "<td colspan=\"3\" bgcolor=\"#FFFFFF\"><a href=\"(.*)</td>"
+hackcnurl = "target=\"_blank\">(.*)</a>"
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/48.0.2564.116 Safari/537.36'}
 
 
-def geticp(url):
-    # url = "qq.com"
+def getIcp(url):
+    """
+    :param url: String
+    :return: Array
+
+    """
     s = requests.session()
     r = s.get('http://icp.chinaz.com/info?q=' + url, headers=headers, timeout=20)
     n = "class=\"by1\">(.*)</td>"
@@ -27,74 +39,71 @@ def geticp(url):
         print(url, '未备案')
 
 
-def gethacker(page):
+def getHack(hack):
+    """
+    :param hack: Int
+        1->www.hac-ker.net
+        2->www.hack-cn.com
+    :return: Array
+        UrlArray
+    """
+    global RUN
+    global COUNT
+    RUN = True
+    COUNT = 1
+    urlArr = []
     day = time.strftime("%Y-%m-%d", time.localtime())
-    url = "http://www.hac-ker.net/?page=" + str(page)
-    s = requests.session()
-    r = s.get(url, headers=headers)
-
-    # p = re.compile('\s+')
-    # html = re.sub(p, '', r.text)
-    tm = "<td width=\"12%\"><b style=\"color:#0BA81E;\">(.*)</td>"
-    url = "<td width=\"58%\" style=\"word-break:break-all\"><a href=\"(.*)\" target=\"_blank\""
-    # tr = "<tr>(.*)</tr>"
-    alltm = re.findall(tm, r.text)
-    allurl = re.findall(url, r.text)
-    i = 0
-    for a in alltm:
-        if a == day:
-            geticp(get_tld(allurl[i]))
-            getpic(allurl[i])
-            ip = socket.gethostbyname(urlparse(allurl[i], scheme='', allow_fragments=True).netloc)
-            print(ip)
+    while RUN:
+        if hack == 1:
+            print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
+            url = "http://www.hac-ker.net/?page=" + str(COUNT)
+            s = requests.session()
+            r = s.get(url, headers=headers, timeout=20)
+            print(time.strftime("%H:%M:%S", time.localtime()), "读取完毕,开始处理.")
+            alltm = re.findall(hackertime, r.text)
+            allurl = re.findall(hackerurl, r.text)
+            i = 0
+            for a in alltm:
+                if a == day:
+                    urlArr.append(allurl[i])
+                else:
+                    RUN = False
+                    print(time.strftime("%H:%M:%S", time.localtime()), "Over")
+                    return urlArr
+                    # return 'over2'
+                i += 1
+            COUNT += 1
+        elif hack == 2:
+            print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
+            url = "http://www.hack-cn.com/?page=" + str(COUNT)
+            s = requests.session()
+            r = s.get(url, headers=headers)
+            print(time.strftime("%H:%M:%S", time.localtime()), "读取完毕,开始处理.")
+            alltm = re.findall(hackcntime, r.text)
+            allurl = re.findall(hackcnweb, r.text)
+            i = 0
+            for a in alltm:
+                if a == day:
+                    url = "http://www.hack-cn.com/" + str(allurl[i])
+                    s = requests.session()
+                    r = s.get(url, headers=headers, timeout=20)
+                    allurl = re.findall(hackcnu, r.text)
+                    allurl = re.findall(hackcnurl, allurl[0])
+                    urlArr.append(allurl[0])
+                else:
+                    RUN = False
+                    print(time.strftime("%H:%M:%S", time.localtime()), "Over")
+                    return urlArr
+                i += 1
+            COUNT += 1
         else:
-            print('over')
-            return 'over2'
-        i += 1
-    gethacker(int(page) + 1)
+            RUN = False
+            break
+
+    return urlArr
 
 
-def gethackcn(page):
-    day = time.strftime("%Y-%m-%d", time.localtime())
-    url = "http://www.hack-cn.com/?page=" + str(page)
-    s = requests.session()
-    r = s.get(url, headers=headers)
-
-    # p = re.compile('\s+')
-    # html = re.sub(p, '', r.text)
-    tm = "<td width=\"16%\">(.*)</td>"
-    web = "<td width=\"7%\"><a href=\"(.*)\" target=\"_blank\">View</a></td>"
-    alltm = re.findall(tm, r.text)
-    allweb = re.findall(web, r.text)
-
-    i = 0
-    for a in alltm:
-        if a == day:
-            gethackcndata(allweb[i])
-        else:
-            print('over')
-            return 'over2'
-        i += 1
-    gethackcn(int(page) + 1)
-
-
-def gethackcndata(web):
-    url = "http://www.hack-cn.com/" + str(web)
-    s = requests.session()
-
-    r = s.get(url, headers=headers)
-    url = "<td colspan=\"3\" bgcolor=\"#FFFFFF\"><a href=\"(.*)</td>"
-    u = "target=\"_blank\">(.*)</a>"
-    allurl = re.findall(url, r.text)
-    allurl = re.findall(u, allurl[0])
-    geticp(get_tld(allurl[0]))
-    ip = socket.gethostbyname(urlparse(allurl[0], scheme='', allow_fragments=True).netloc)
-    print(ip)
-    print(allurl[0])
-    getpic(allurl[0])
-
-
-def verify(url):
+def Verify(url):
     s = requests.session()
     r = s.get(url, headers=headers, timeout=20)
     if r.status_code == 200 and r.text != '':
@@ -105,21 +114,38 @@ def verify(url):
         return r.status_code
 
 
-def getpic(url):
+def getPic(url):
     tm = time.strftime("%m-%d %H-%M-%S", time.localtime())
-    na = str(str(tm) + ' ' + url + '.jpg').replace('http://', '').replace('https://', '').replace(':', ' ').replace('/', '_').replace('?', '%3F')
-    print(na)
+    dir = time.strftime("%m-%d", time.localtime())
+    if os.path.exists("./img/"):
+        if os.path.exists("./img/" + dir + "/"):
+            print("dir")
+        else:
+            os.mkdir("./img/" + dir + "/")
+    else:
+        os.mkdir("./img/")
+        if os.path.exists("./img/" + dir + "/"):
+            print("dir")
+        else:
+            os.mkdir("./img/" + dir + "/")
+
+    na = str(str(tm) + ' ' + url + '.jpg').replace('http://', '').replace('https://', '').replace(':', ' ')\
+        .replace('/', '_').replace('?', '%3F')
     browser = webdriver.Chrome()
     browser.set_page_load_timeout(20)
     browser.implicitly_wait(20)
     try:
         browser.get(url)
-        browser.save_screenshot("./img/123.jps")
+        browser.save_screenshot("./img/" + dir + "/" + na)
         browser.quit()
     except TimeoutException:
         print('time out after 20 seconds when loading page')
+        browser.save_screenshot("./img/" + dir + "/" + na)
+        browser.quit()
         browser.execute_script('window.stop()')
 
 
-gethacker(1)
+urlArr = getHack(1)
+for a in urlArr:
+    print(a)
 # getpic('http://status.zer.moe:89/webstatus/hebei')
