7f8ffcb69c83bdc8ae68f055f4799aeac6dc02a3
GhostStar <root@zer.moe>
2016-03-18 10:01:56 +0800

    LINUX下截图

--- hackspider.py
+++ hackspider.py
@@ -5,16 +5,35 @@ import os
 from tld import get_tld
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+import platform
 
+
+def isLINUX():
+    """
+    取系统是否为LINUX
+    """
+    global LINUX
+    if 'Windows' in platform.system():
+        LINUX = False
+    elif 'Linux' in platform.system():
+        LINUX = True
+    else:
+        print('系统无法识别可能无法截图.')
+
+# 判断是否为LINUX
+isLINUX()
+# Get Post以及截图的超时时间
+TIMEOUT = 20
+# 获取时间和url的规则
 hackertime = "<td width=\"12%\"><b style=\"color:#0BA81E;\">(.*)</td>"
 hackerurl = "<td width=\"58%\" style=\"word-break:break-all\"><a href=\"(.*)\" target=\"_blank\""
 hackcntime = "<td width=\"16%\">(.*)</td>"
 hackcnweb = "<td width=\"7%\"><a href=\"(.*)\" target=\"_blank\">View</a></td>"
 hackcnu = "<td colspan=\"3\" bgcolor=\"#FFFFFF\"><a href=\"(.*)</td>"
 hackcnurl = "target=\"_blank\">(.*)</a>"
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/48.0.2564.116 Safari/537.36'}
+# User-Agent
+HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/48.0.2564.116 Safari/537.36'}
 
 
 def getIcp(url):
@@ -30,7 +49,7 @@ def getIcp(url):
     url = get_tld(url)
     arr = []
     s = requests.session()
-    r = s.get('http://icp.chinaz.com/info?q=' + url, headers=headers, timeout=20)
+    r = s.get('http://icp.chinaz.com/info?q=' + url, headers=HEADER, timeout=TIMEOUT)
     n = "class=\"by1\">(.*)</td>"
     b = "class=\"by1\" width=\"30%\">(.*)</td>"
     by = "<td align=\"left\" class=\"by2\">(.*)</td>"
@@ -55,7 +74,7 @@ def getHack(hack):
     global RUN
     global COUNT
     RUN = True
-    COUNT = 1
+    COUNT = 1  # 页
     urlArr = []
     day = time.strftime("%Y-%m-%d", time.localtime())
     while RUN:
@@ -63,8 +82,8 @@ def getHack(hack):
             print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
             url = "http://www.hac-ker.net/?page=" + str(COUNT)
             s = requests.session()
-            r = s.get(url, headers=headers, timeout=20)
-            print(time.strftime("%H:%M:%S", time.localtime()), "读取完毕,开始处理.")
+            r = s.get(url, headers=HEADER, timeout=TIMEOUT)
+            print(time.strftime("%H:%M:%S", time.localtime()), "hacker读取完毕,开始处理.")
             alltm = re.findall(hackertime, r.text)
             allurl = re.findall(hackerurl, r.text)
             i = 0
@@ -73,7 +92,7 @@ def getHack(hack):
                     urlArr.append(allurl[i])
                 else:
                     RUN = False
-                    print(time.strftime("%H:%M:%S", time.localtime()), "Over")
+                    print(time.strftime("%H:%M:%S", time.localtime()), "Url读取完毕")
                     return urlArr
                     # return 'over2'
                 i += 1
@@ -82,8 +101,8 @@ def getHack(hack):
             print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
             url = "http://www.hack-cn.com/?page=" + str(COUNT)
             s = requests.session()
-            r = s.get(url, headers=headers)
-            print(time.strftime("%H:%M:%S", time.localtime()), "读取完毕,开始处理.")
+            r = s.get(url, headers=HEADER)
+            print(time.strftime("%H:%M:%S", time.localtime()), "hackcn列表读取完毕,开始读取Url.")
             alltm = re.findall(hackcntime, r.text)
             allurl = re.findall(hackcnweb, r.text)
             i = 0
@@ -91,13 +110,13 @@ def getHack(hack):
                 if a == day:
                     url = "http://www.hack-cn.com/" + str(allurl[i])
                     s = requests.session()
-                    r = s.get(url, headers=headers, timeout=20)
+                    r = s.get(url, headers=HEADER, timeout=TIMEOUT)
                     blackurl = re.findall(hackcnu, r.text)
                     blackurl = re.findall(hackcnurl, blackurl[0])
                     urlArr.append(blackurl[0])
                 else:
                     RUN = False
-                    print(time.strftime("%H:%M:%S", time.localtime()), "Over")
+                    print(time.strftime("%H:%M:%S", time.localtime()), "Url读取完毕")
                     return urlArr
                 i += 1
             COUNT += 1
@@ -109,22 +128,28 @@ def getHack(hack):
 
 
 def Verify(url):
+    """
+    调用站长之家获取网站IP和状态码
+    """
     s = requests.session()
     data = {'url': url}
     fi = "<div class=\"fr zTContrig\"><span>(.*)</span></div></li><li class=\"bor-b1s bg-list clearfix\">" \
          "<div class=\"fl zTContleft\">返回状态码</div><div class=\"fr zTContrig\"><span>(.*)</span></div></li>"
-    r = s.get("http://tool.chinaz.com/pagestatus/", data=data, headers=headers, timeout=20)
+    r = s.get("http://tool.chinaz.com/pagestatus/", data=data, headers=HEADER, timeout=TIMEOUT)
     if r.text.find("检测结果") != -1:
         ret = re.findall(fi, r.text)
-        for ser in ret:  #########
-            return ser
+        return ret
     else:
         ser = []
         return ser
 
 
 def getPic(url):
-    return url
+    """
+    Windows调用chromedriver.exe截图
+    Linux调用CutyCapt截图 (需预先安装)(Ubuntu14.04下测试正常)
+    :param url: http://google.com
+    """
     tm = time.strftime("%m-%d %H-%M-%S", time.localtime())
     dire = time.strftime("%m-%d", time.localtime())
     if os.path.exists("./img/"):
@@ -135,43 +160,58 @@ def getPic(url):
         if not os.path.exists("./img/" + dire + "/"):
             os.mkdir("./img/" + dire + "/")
 
-    na = str(str(tm) + ' ' + url + '.jpg').replace('http://', '').replace('https://', '').replace(':', ' ') \
+    na = str(str(tm) + ' ' + url + '.png').replace('http://', '').replace('https://', '').replace(':', ' ') \
         .replace('/', '_').replace('?', '%3F')
-    browser = webdriver.Chrome()
-    browser.set_page_load_timeout(20)
-    browser.implicitly_wait(20)
-    try:
-        browser.get(url)
-        browser.save_screenshot("./img/" + dire + "/" + na)
-        browser.quit()
-    except TimeoutException:
-        print('time out after 20 seconds when loading page')
-        browser.save_screenshot("./img/" + dire + "/" + na)
-        browser.quit()
-        browser.execute_script('window.stop()')
-    return na
+    na = "./img/" + dire + "/" + na
+
+    if not LINUX:
+        browser = webdriver.Chrome()
+        browser.set_page_load_timeout(TIMEOUT)
+        browser.implicitly_wait(TIMEOUT)
+        try:
+            browser.get(url)
+            browser.save_screenshot(na)
+            browser.quit()
+        except TimeoutException:
+            print('time out after 20 seconds when loading page')
+            browser.save_screenshot(na)
+            browser.quit()
+            # browser.execute_script('window.stop()')
+        return na
+    else:
+        wait = str(TIMEOUT * 1000)
+        ret = os.system('xvfb-run --server-args="-screen 0, 1280x1200x24" cutycapt --max-wait=' + wait +
+                        ' --url=\'' + url + '\' --out=\'' + na + '\'')
+        if ret == 0:
+            return na
+        else:
+            return False
 
 
 def echo(urlArr):
+    """
+    获取Url的IP 响应码 备案 以及截图
+    :type urlArr: Url数组
+    """
     for a in urlArr:
         print(a, "查询状态...")
         ser = Verify(a)
         if not ser:
-            if ser[1] != '404':
+            print(a, "无法解析或访问,跳过...")
+        else:
+            print(ser)
+            if ser[0][1] != '404':
                 print(a, "查询备案...")
                 icp = getIcp(a)
                 print(a, "获取截图...")
                 pic = getPic(a)
                 if not icp:
-                    print(a, "Ip:", ser[0], "Status:", ser[1], "没有备案", "Screenshot:", pic)
+                    print(a, "Ip:", ser[0][0], "Status:", ser[0][1], "没有备案", "Screenshot:", pic)
                 else:
-                    print(a, "Ip:", ser[0], "Status:", ser[1], "Com:", icp[0], "Name:", icp[1], "Number:", icp[2],
+                    print(a, "Ip:", ser[0][0], "Status:", ser[0][1], "Com:", icp[0], "Name:", icp[1], "Number:", icp[2],
                           "Property:", icp[3], "Screenshot:", pic)
             else:
                 print(a, "地址404,跳过...")
-        else:
-            print(a, "无法解析或访问,跳过...")
-
     print('done')
 
 
