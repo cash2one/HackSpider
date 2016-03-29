101721129ffbea67538324c8bfe02fbc5328be0b
GhostStar <623128489@qq.com>
2016-03-29 09:56:32 +0800

    fix

--- hackspider.py
+++ hackspider.py
@@ -9,246 +9,335 @@ from tld import get_tld
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 import platform
+from sqlalchemy import Column, create_engine
+from sqlalchemy.types import CHAR, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-def isLINUX():
-    """
-    取系统是否为LINUX
-    """
-    global LINUX
-    if 'Windows' in platform.system():
-        LINUX = False
-    elif 'Linux' in platform.system():
-        LINUX = True
-    else:
-        print('系统无法识别可能无法截图.')
+class Hack_spider(Base):  # 分组表
+    __tablename__ = 'hack_spider'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    domain = Column(String(500), unique=True)  # 域名
+    hackweb = Column(String(500))  # 黑页
+    ip = Column(String(500))  # IP
+    icp = Column(String(500), default="")  # ICP
+    icp_name = Column(String(500), default=None)  # 备案单位
+    icp_webname = Column(String(500), default=None)  # 备案网站名
+    icp_st = Column(String(500), default=None)  # 备案类型
+    city = Column(String(500))  # 归属地
+    time = Column(Integer)  # 时间
+    pic = Column(String(500))  # 快照路径
+    origin = Column(String(20))  # 采集来源
 
 
-# 判断是否为LINUX
-isLINUX()
-# Get Post以及截图的超时时间
-TIMEOUT = 20
-# 获取时间和url的规则
-hackertime = "<td width=\"12%\"><b style=\"color:#0BA81E;\">(.*)</td>"
-hackerurl = "<td width=\"58%\" style=\"word-break:break-all\"><a href=\"(.*)\" target=\"_blank\""
-hackcntime = "<td width=\"16%\">(.*)</td>"
-hackcnweb = "<td width=\"7%\"><a href=\"(.*)\" target=\"_blank\">View</a></td>"
-hackcnu = "<td colspan=\"3\" bgcolor=\"#FFFFFF\"><a href=\"(.*)</td>"
-hackcnurl = "target=\"_blank\">(.*)</a>"
-# User-Agent
-HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                        'Chrome/48.0.2564.116 Safari/537.36'}
-
-
-def getIcp(url):
-    """
-    # 获取网站备案
-    :param url: String
-    :return: Array
-    Arr[单位名称,网站名称,备案号,备案性质]
-
-    # url = "http://icp.chinaz.com/searchs"
-    # data = {'urls': 'qq.com', 'btn_search': '查询'}
-
-    """
-    url = get_tld(url)
-    arr = []
-    s = requests.session()
-    r = s.get('http://icp.chinaz.com/info?q=' + url, headers=HEADER, timeout=TIMEOUT)
-    n = "class=\"by1\">(.*)</td>"
-    b = "class=\"by1\" width=\"30%\">(.*)</td>"
-    by = "<td align=\"left\" class=\"by2\">(.*)</td>"
-    if r.text.find('错误') == -1:
-        name = re.findall(n, r.text)
-        num = re.findall(b, r.text)
-        by = re.findall(by, r.text)
-        arr = [name[0], num[1], name[2], by[0]]
-        return arr
-    else:
-        return arr
-
-
-def getHack(hack):
-    """
-    # 获取今日被黑网页
-    :param hack: Int
-        1->www.hac-ker.net
-        2->www.hack-cn.com
-    :return: Array
-        UrlArray
-    """
-    global RUN
-    global COUNT
-    RUN = True
-    COUNT = 1  # 页
-    urlArr = []
-    day = time.strftime("%Y-%m-%d", time.localtime())
-    while RUN:
-        if hack == 1:
-            print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
-            url = "http://www.hac-ker.net/?page=" + str(COUNT)
-            s = requests.session()
-            r = s.get(url, headers=HEADER, timeout=TIMEOUT)
-            print(time.strftime("%H:%M:%S", time.localtime()), "hacker读取完毕,开始处理.")
-            alltm = re.findall(hackertime, r.text)
-            allurl = re.findall(hackerurl, r.text)
-            i = 0
-            for a in alltm:
-                if a == day:
-                    urlArr.append(allurl[i])
-                else:
-                    RUN = False
-                    print(time.strftime("%H:%M:%S", time.localtime()), "Url读取完毕")
-                    return urlArr
-                    # return 'over2'
-                i += 1
-            COUNT += 1
-        elif hack == 2:
-            print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
-            url = "http://www.hack-cn.com/?page=" + str(COUNT)
-            s = requests.session()
-            r = s.get(url, headers=HEADER)
-            print(time.strftime("%H:%M:%S", time.localtime()), "hackcn列表读取完毕,开始读取Url.")
-            alltm = re.findall(hackcntime, r.text)
-            allurl = re.findall(hackcnweb, r.text)
-            i = 0
-            for a in alltm:
-                if a == day:
-                    url = "http://www.hack-cn.com/" + str(allurl[i])
-                    s = requests.session()
-                    r = s.get(url, headers=HEADER, timeout=TIMEOUT)
-                    blackurl = re.findall(hackcnu, r.text)
-                    blackurl = re.findall(hackcnurl, blackurl[0])
-                    urlArr.append(blackurl[0])
-                else:
-                    RUN = False
-                    print(time.strftime("%H:%M:%S", time.localtime()), "Url读取完毕")
-                    return urlArr
-                i += 1
-            COUNT += 1
+class hackspider:
+    def __init__(self):
+        # 判断是否为LINUX
+        if 'Windows' in platform.system():
+            self.LINUX = False
+        elif 'Linux' in platform.system():
+            self.LINUX = True
         else:
-            RUN = False
-            break
+            print('系统无法识别可能无法截图.')
+            self.LINUX = False
 
-    return urlArr
+        self.SAVE_SQL = True
+        self.DEBUG_MODE = False
+        self.DEBUG_TIME = "2016-03-28"
+        self.PRTSC = True
 
+        # --------------------------------------------
+        self.SQL_HOST = '127.0.0.1'
+        self.SQL_USER = 'app'
+        self.SQL_PASS = ''
+        self.SQL_DB = 'app_webstatus'
+        self.SQL_PORT = 3306
+        self.SQL_CHAR = 'utf8'
+        engine = create_engine(
+            "mysql+pymysql://" + self.SQL_USER + ":" + self.SQL_PASS + "@" + self.SQL_HOST + "/" + self.SQL_DB +
+            "?charset=utf8", echo=False)
 
+        self.DBSession = sessionmaker(bind=engine)
 
-def getPic(url):
-    """
-    # 获取网站截图
-    Windows调用chromedriver.exe截图
-    Linux调用CutyCapt截图 (需预先安装)(Ubuntu14.04下测试正常)
-    :param url: http://google.com
-    """
-    tm = time.strftime("%m-%d %H-%M-%S", time.localtime())
-    dire = time.strftime("%m-%d", time.localtime())
-    if os.path.exists("./img/"):
-        if not os.path.exists("./img/" + dire + "/"):
-            os.mkdir("./img/" + dire + "/")
-    else:
-        os.mkdir("./img/")
-        if not os.path.exists("./img/" + dire + "/"):
-            os.mkdir("./img/" + dire + "/")
+        # Get Post以及截图的超时时间
+        self.TIMEOUT = 20
+        # User-Agent
+        self.HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                     'Chrome/48.0.2564.116 Safari/537.36'}
+        # 获取时间和url的规则
+        # self.hackertime = "<td width=\"12%\"><b style=\"color:#0BA81E;\">(.*)</td>"
+        self.hackerurl = "<td width=\"58%\" style=\"word-break:break-all\"><a href=\"(.*)\" target=\"_blank\""
+        # self.hackcntime = "<td width=\"16%\">(.*)</td>"
+        self.hackcnweb = "<td width=\"7%\"><a href=\"(.*)\" target=\"_blank\">View</a></td>"
+        self.hackcnu = "<td colspan=\"3\" bgcolor=\"#FFFFFF\"><a href=\"(.*)</td>"
+        self.hackcnurl = "target=\"_blank\">(.*)</a>"
 
-    na = str(str(tm) + ' ' + url + '.png').replace('http://', '').replace('https://', '').replace(':', ' ') \
-        .replace('/', '_').replace('?', '%3F')
-    na = "./img/" + dire + "/" + na
+    def getIcp(self, url):
+        """
+        # 获取网站备案
+        :param url: String
+        :return: Array
+        Arr[单位名称,网站名称,备案号,备案性质]
 
-    if not LINUX:
-        browser = webdriver.Chrome()
-        browser.set_page_load_timeout(TIMEOUT)
-        browser.implicitly_wait(TIMEOUT)
+        # url = "http://icp.chinaz.com/searchs"
+        # data = {'urls': 'qq.com', 'btn_search': '查询'}
+
+        """
         try:
-            browser.get(url)
-            browser.save_screenshot(na)
-            browser.quit()
-        except TimeoutException:
-            print('time out after 20 seconds when loading page')
-            browser.save_screenshot(na)
-            browser.quit()
-            # browser.execute_script('window.stop()')
-        return na
-    else:
-        wait = str(TIMEOUT * 1000)
-        ret = os.system('xvfb-run --server-args="-screen 0, 1280x1200x24" cutycapt --max-wait=' + wait +
-                        ' --url=\'' + url + '\' --out=\'' + na + '\'')
-        if ret == 0:
+            url = get_tld(url)
+        except Exception as e:
+            print(e)
+        arr = []
+        s = requests.session()
+        r = s.get('http://icp.chinaz.com/info?q=' + url, headers=self.HEADER, timeout=self.TIMEOUT)
+        n = "class=\"by1\">(.*)</td>"
+        b = "class=\"by1\" width=\"30%\">(.*)</td>"
+        by = "<td align=\"left\" class=\"by2\">(.*)</td>"
+        if r.text.find('错误') == -1:
+            name = re.findall(n, r.text)
+            num = re.findall(b, r.text)
+            by = re.findall(by, r.text)
+            arr = [name[0], num[1], name[2], by[0]]
+            return arr
+        else:
+            return arr
+
+    def getHack(self, hack, last):
+        """
+        # 获取今日被黑网页
+        :param hack: Int
+            1->www.hac-ker.net
+            2->www.hack-cn.com
+        :return: Array
+            UrlArray
+        """
+        global RUN
+        global COUNT
+        RUN = True
+        COUNT = 1  # 页
+        urlArr = []
+        s = requests.session()
+        while RUN:
+            if hack == 1:
+                print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
+                url = "http://www.hac-ker.net/?page=" + str(COUNT)
+                r = s.get(url, headers=self.HEADER, timeout=self.TIMEOUT)
+                print(time.strftime("%H:%M:%S", time.localtime()), "hacker读取完毕,开始处理.")
+                allurl = re.findall(self.hackerurl, r.text)
+                i = 0
+
+                for a in allurl:
+                    if a != last:
+                        urlArr.append(a)
+                    else:
+                        RUN = False
+                        print(time.strftime("%H:%M:%S", time.localtime()), "Url读取完毕")
+                        urlArr.reverse()
+                        return urlArr
+                    i += 1
+                COUNT += 1
+            elif hack == 2:
+                print(time.strftime("%H:%M:%S", time.localtime()), "第" + str(COUNT) + "页读取开始请稍候...")
+                url = "http://www.hack-cn.com/?page=" + str(COUNT)
+
+                r = s.get(url, headers=self.HEADER)
+                print(time.strftime("%H:%M:%S", time.localtime()), "hackcn列表读取完毕,开始读取Url.")
+                allurl = re.findall(self.hackcnweb, r.text)
+                i = 0
+
+                for a in allurl:
+                    if a != last:
+                        url = "http://www.hack-cn.com/" + str(a)
+                        r = s.get(url, headers=self.HEADER, timeout=self.TIMEOUT)
+                        blackurl = re.findall(self.hackcnu, r.text)
+                        blackurl = re.findall(self.hackcnurl, blackurl[0])
+                        urlArr.append(blackurl[0])
+                    else:
+                        RUN = False
+                        print(time.strftime("%H:%M:%S", time.localtime()), "Url读取完毕")
+                        urlArr.reverse()
+                        return urlArr
+                    i += 1
+                COUNT += 1
+            else:
+                RUN = False
+                break
+        urlArr.reverse()
+        return urlArr
+
+    def getPic(self, url):
+        """
+        # 获取网站截图
+        Windows调用chromedriver.exe截图
+        Linux调用CutyCapt截图 (需预先安装)(Ubuntu14.04下测试正常)
+        :param url: http://google.com
+        """
+        # tm = time.strftime("%m-%d %H-%M-%S", time.localtime())
+        dire = time.strftime("%m-%d", time.localtime())
+        if os.path.exists("./hackimg/"):
+            if not os.path.exists("./hackimg/" + dire + "/"):
+                os.mkdir("./hackimg/" + dire + "/")
+        else:
+            os.mkdir("./hackimg/")
+            if not os.path.exists("./hackimg/" + dire + "/"):
+                os.mkdir("./hackimg/" + dire + "/")
+
+        # na = str(str(tm) + ' ' + url + '.png').replace('http://', '').replace('https://', '').replace(':', ' ') \
+        #     .replace('/', '_').replace('?', '%3F')
+        na = self.getDomain(url) + '.png'
+        na = "./hackimg/" + dire + "/" + na
+
+        if not self.LINUX:
+            browser = webdriver.Chrome()
+            # browser = webdriver.Chrome()
+            browser.set_page_load_timeout(self.TIMEOUT)
+            browser.implicitly_wait(self.TIMEOUT)
+            try:
+                browser.get(url)
+                print(browser.title)
+                browser.save_screenshot(na)
+                browser.quit()
+            except TimeoutException:
+                print('time out after 20 seconds when loading page')
+                browser.save_screenshot(na)
+                browser.quit()
+                # browser.execute_script('window.stop()')
             return na
         else:
-            return False
-
-
-def echo(urlArr):
-    """
-    # 获取Url的IP 响应码 备案 以及截图
-    :type urlArr: Url数组
-    """
-    for a in urlArr:
-        print(a, "查询状态...")
-        ser = Verify(a)
-        if ser:
-            print(a, "查询备案...")
-            icp = getIcp(a)
-            print(a, "获取截图...")
-            pic = getPic(a)
-            if not icp:
-                print(a, "Ip:", ser[0], "city:", ser[1], "没有备案", "Screenshot:", pic)
-            else:
-                print(a, "Ip:", ser[0], "city:", ser[1], "Com:", icp[0], "Name:", icp[1], "Number:", icp[2],
-                      "Property:", icp[3], "Screenshot:", pic)
-        else:
-            print(a, "无法访问，跳过...")
-    print('done')
-
-
-def Verify(url):
-    """
-    # 验证网站
-    :param url:
-    :return:
-    """
-    doom = isCHINA(url)
-    if doom:
-        try:
-            s = requests.session()
-            r = str(s.head(url, headers=HEADER, timeout=TIMEOUT))
-            print(r)
-            if r.find("200") != -1 or r.find("301") != -1 or r.find("302") != -1:
-                return doom
+            wait = str(self.TIMEOUT * 1000)
+            ret = os.system('xvfb-run --server-args="-screen 0, 1280x1200x24" cutycapt --max-wait=' + wait +
+                            ' --url=\'' + url + '\' --out=\'' + na + '\'')
+            if ret == 0:
+                return na
             else:
                 return False
-        except Exception:
-            return False
-    else:
-        return False
 
-
-def getDomain(url):
-    return urlparse(url, scheme='', allow_fragments=True).netloc
-
-
-def isCHINA(url):
-    """
-    # 获取IP 并判断是否为国内网站 返回IP和归属地
-    :param url:
-    :return:
-    """
-    ret = []
-    try:
-        ip = socket.gethostbyname(getDomain(url))
-        ser = IP.find(ip)
-        if not ser:
-            return
-        else:
-            if ser.find('中国') != -1 or ser.find('香港') != -1:
-                ret.append(ip)
-                ret.append(ser)
-                return ret
+    def echo(self, urlArr, hackOrigin):
+        """
+        # 获取Url的IP 响应码 备案 以及截图
+        :type urlArr: Url数组
+        """
+        for a in urlArr:
+            print(a, "查询状态...")
+            ser = self.Verify(a)
+            if ser:
+                print(a, "查询备案...")
+                icp = self.getIcp(a)
+                if self.PRTSC:
+                    print(a, "获取截图...")
+                    pic = self.getPic(a)
+                else:
+                    pic = ""
+                if not icp:
+                    if self.SAVE_SQL:
+                        self.addsql(self.getDomain(a), a, ser[0], "", "", "", "", ser[1], time.time(), pic, hackOrigin)
+                        print(a, "Domain:", self.getDomain(a), "Ip:", ser[0], "city:", ser[1], "没有备案", "Screenshot:",
+                              pic)
+                    else:
+                        print(a, "Domain:", self.getDomain(a), "Ip:", ser[0], "city:", ser[1], "没有备案", "Screenshot:",
+                              pic)
+                else:
+                    if self.SAVE_SQL:
+                        # domain, hackweb, ip, icp, icp_name, icp_webname, icp_st, city, time, pic, origin
+                        self.addsql(self.getDomain(a), a, ser[0], icp[2], icp[0], icp[1], icp[3], ser[1], time.time(),
+                               pic, hackOrigin)
+                        print(a, "Domain:", self.getDomain(a), "Ip:", ser[0], "city:", ser[1], "Com:", icp[0], "Name:",
+                              icp[1], "Number:", icp[2], "Property:", icp[3], "Screenshot:", pic)
+                    else:
+                        print(a, "Domain:", self.getDomain(a), "Ip:", ser[0], "city:", ser[1], "Com:", icp[0], "Name:",
+                              icp[1], "Number:", icp[2], "Property:", icp[3], "Screenshot:", pic)
             else:
-                return
-    except Exception:
-        return
+                print(a, "无法访问或者不是国内网站,跳过...")
+        print('done')
 
-echo(getHack(1))
-echo(getHack(2))
+    def Verify(self, url):
+        """
+        # 验证网站
+        :param url:
+        :return:
+        """
+        doom = self.isCHINA(url)
+        if doom:
+            try:
+                s = requests.session()
+                r = s.head(url, headers=self.HEADER, timeout=self.TIMEOUT)
+
+                if r.status_code != 200 or r.status_code != 301 or r.status_code != 302:
+                    return doom
+                else:
+                    return False
+            except Exception:
+                return False
+        else:
+            return False
+
+    def getDomain(self, url):
+        return urlparse(url, scheme='', allow_fragments=True).netloc
+
+    def isCHINA(self, url):
+        """
+        # 获取IP 并判断是否为国内网站 返回IP和归属地
+        :param url:
+        :return:
+        """
+        ret = []
+        try:
+            ip = socket.gethostbyname(self.getDomain(url))
+            ser = IP.find(ip)
+            if not ser:
+                return
+            else:
+                if ser.find('中国') != -1:
+                    ret.append(ip)
+                    ret.append(ser)
+                    return ret
+                else:
+                    return
+        except Exception as e:
+            print(e)
+            return
+
+    def addsql(self, domain, hackweb, ip, icp, icp_name, icp_webname, icp_st, city, time, pic, origin):
+        """
+        # 存入数据库
+        """
+        try:
+            session = self.DBSession()
+            sql = Hack_spider(domain=domain, hackweb=hackweb, ip=ip, icp=icp, icp_name=icp_name,
+                              icp_webname=icp_webname,
+                              icp_st=icp_st, city=city, time=int(time), pic=pic, origin=origin)
+            session.add(sql)
+            session.commit()
+            session.close()
+        except Exception as e:
+            print(e)
+
+    def getlastsql(self, origin):
+        """
+        获取最后一次入库的记录
+        """
+        session = self.DBSession()
+        query = session.query(Hack_spider)
+        ret = query.filter(Hack_spider.origin == origin).order_by(Hack_spider.id.desc()).first()
+        return ret.hackweb
+
+
+if __name__ == '__main__':
+    sp = hackspider()
+    sp.SAVE_SQL = False
+    sp.PRTSC = False
+
+    origin = sp.getlastsql("1")
+    print(origin)
+    sp.echo(sp.getHack(1, origin), "1")
+
+'''
+
+    sp.echo(sp.getHack(1), "1")
+    sp.echo(sp.getHack(2), "2")
+    '''
+# echo(getHack(1))
+# echo(getHack(2))
